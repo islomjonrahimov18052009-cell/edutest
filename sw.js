b7db52d099a2fa7252b2a51b6bbdbae87ab59217
islomjonrahimov18052009-cell <islomjonrahimov18.05.2009@gmail.com>
2026-06-21 13:42:36 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'edutest-v1';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES).catch(function(){});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
